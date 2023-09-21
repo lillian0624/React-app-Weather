dccfaf7e2d59ea5ae80fceb48776b0f45af8c7c3
lillian0624 <nopeyangli@163.com>
2023-09-21 23:23:25 +1000

    5-day forecast in each 3 hours

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from "react";
 import "./App.css";
 import Weather from "./Components/Weather";
+import Forecast from "./Components/Forecast";
 
 const API_KEY = "621628665af6bfd2cd7613c424e1a525";
-const CITY = "Sydney";
+const CITY = "Canberra";
 
 function App() {
   return (
     <div className="App">
-      <Weather apiKey={API_KEY} city={CITY}></Weather>
+      <Weather apiKey={API_KEY} city={CITY} />
+      <Forecast apiKey={API_KEY} city={CITY} />
     </div>
   );
 }
--- src/Components/Forecast.js
+++ src/Components/Forecast.js
@@ -1,1 +1,49 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const Forecast = ({ apiKey, city }) => {
+  const [forecastData, setForecastData] = useState(null);
+
+  useEffect(() => {
+    const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+
+    axios
+      .get(apiUrl)
+      .then((response) => {
+        console.log(response.data);
+        setForecastData(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, [apiKey, city]);
+
+  // Check if forecastData is still null (data not received yet)
+  if (!forecastData) {
+    return <div>Loading...</div>;
+  }
+
+  if (!forecastData.list || forecastData.list.length === 0) {
+    return <div>No forecast data available.</div>;
+  }
+
+  // Extract the list of forecasts from the API response for the next 5 days
+  const forecastList = forecastData.list;
+
+  return (
+    <div>
+      <h2>5-Day Weather Forecast for {city}</h2>
+      <div className="forecast-container">
+        {forecastList.map((forecast, index) => (
+          <div key={index} className="forecast-item">
+            <p>Date: {forecast.dt_txt}</p>
+            <p>Temperature: {forecast.main.temp}°C</p>
+            <p>Weather: {forecast.weather[0].description}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Forecast;
