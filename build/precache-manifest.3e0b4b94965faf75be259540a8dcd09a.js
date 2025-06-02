c4170465cd83492009b2e1c7035263fdfd5176ac
Lillian Yang <nopeyangli@163.com>
2025-06-02 11:41:15 +1000

    "build"

---
+++ build/precache-manifest.3e0b4b94965faf75be259540a8dcd09a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5285970db09342b6648d",
+    "url": "/static/css/main.1e77dc84.chunk.css"
+  },
+  {
+    "revision": "5285970db09342b6648d",
+    "url": "/static/js/main.6facd071.chunk.js"
+  },
+  {
+    "revision": "458211bbcfa5ccd927ce",
+    "url": "/static/js/runtime~main.31fcaf80.js"
+  },
+  {
+    "revision": "984a34d420cb5c03823a",
+    "url": "/static/js/2.2f897f95.chunk.js"
+  },
+  {
+    "revision": "eeb29d83d1bcbaba4e91",
+    "url": "/static/js/3.82db40f9.chunk.js"
+  },
+  {
+    "revision": "8d71cdb6fae14b08f9e79ee132307164",
+    "url": "/index.html"
+  }
+];
